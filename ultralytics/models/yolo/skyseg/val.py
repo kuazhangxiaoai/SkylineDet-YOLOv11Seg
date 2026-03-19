fd8300ddccc1d29eedc8990cc75e93ef4e0872f4
kuazhangxiaoai <yanggang3510@gmail.com>
2026-03-19 13:42:42 +0800

    update validator

--- ultralytics/models/yolo/skyseg/val.py
+++ ultralytics/models/yolo/skyseg/val.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torchmetrics.functional.multimodal.clip_iqa import images
+
 from ultralytics.utils import DEFAULT_CFG, yaml_load,SKYSEG_CFG
 
 from ultralytics.models.yolo.detect import DetectionValidator
@@ -15,7 +17,7 @@ from ultralytics.utils.checks import check_requirements
 from ultralytics.utils.metrics import (SegmentMetrics,SkySegmentMetrics, box_iou, mask_iou, line_eval, SkylineMetrics,
                                        mask_mcr, dice_score, mask_accuracy, mask_recall, mask_precision)
 from ultralytics.utils.plotting import (output_to_target, plot_images,
-                                        plot_eval_images_skyseg, get_edge, plot_eval_images_skyline)
+                                        plot_eval_images_skyseg, get_edge, plot_eval_images_skyline, plot_masks)
 from ultralytics.utils.torch_utils import de_parallel
 from ultralytics.data import build_sky_dataset, build_dataloader
 
@@ -209,11 +211,8 @@ class SkySegmentationValidator(DetectionValidator):
 
     def plot_val_samples(self, batch, ni):
         """Plots validation samples with bounding box labels."""
-        plot_images(
-            batch["img"],
-            batch["batch_idx"],
-            batch["cls"].squeeze(-1),
-            batch["bboxes"],
+        plot_masks(
+            images=batch["img"],
             masks=batch["masks"],
             paths=batch["im_file"],
             fname=self.save_dir / f"val_batch{ni}_labels.jpg",
@@ -223,61 +222,7 @@ class SkySegmentationValidator(DetectionValidator):
 
     def plot_predictions(self, batch, preds, ni):
         """Plots batch predictions with masks and bounding boxes."""
-        plot_images(
-            batch["img"],
-            *output_to_target(preds[0], max_det=15),  # not set to self.args.max_det due to slow plotting speed
-            torch.cat(self.plot_masks, dim=0) if len(self.plot_masks) else self.plot_masks,
-            paths=batch["im_file"],
-            fname=self.save_dir / f"val_batch{ni}_pred.jpg",
-            names=self.names,
-            on_plot=self.on_plot,
-        )  # pred
         self.plot_masks.clear()
-
-    def save_one_txt(self, predn, pred_masks, save_conf, shape, file):
-        """Save YOLO detections to a txt file in normalized coordinates in a specific format."""
-        from ultralytics.engine.results import Results
-
-        Results(
-            np.zeros((shape[0], shape[1]), dtype=np.uint8),
-            path=None,
-            names=self.names,
-            boxes=predn[:, :6],
-            masks=pred_masks,
-        ).save_txt(file, save_conf=save_conf)
-
-    def pred_to_json(self, predn, filename, pred_masks):
-        """
-        Save one JSON result.
-
-        Examples:
-             >>> result = {"image_id": 42, "category_id": 18, "bbox": [258.15, 41.29, 348.26, 243.78], "score": 0.236}
-        """
-        from pycocotools.mask import encode  # noqa
-
-        def single_encode(x):
-            """Encode predicted masks as RLE and append results to jdict."""
-            rle = encode(np.asarray(x[:, :, None], order="F", dtype="uint8"))[0]
-            rle["counts"] = rle["counts"].decode("utf-8")
-            return rle
-
-        stem = Path(filename).stem
-        image_id = int(stem) if stem.isnumeric() else stem
-        box = ops.xyxy2xywh(predn[:, :4])  # xywh
-        box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
-        pred_masks = np.transpose(pred_masks, (2, 0, 1))
-        with ThreadPool(NUM_THREADS) as pool:
-            rles = pool.map(single_encode, pred_masks)
-        for i, (p, b) in enumerate(zip(predn.tolist(), box.tolist())):
-            self.jdict.append(
-                {
-                    "image_id": image_id,
-                    "category_id": self.class_map[int(p[5])],
-                    "bbox": [round(x, 3) for x in b],
-                    "score": round(p[4], 5),
-                    "segmentation": rles[i],
-                }
-            )
 
     def eval_json(self, stats):
         """Return COCO-style object detection evaluation metrics."""
